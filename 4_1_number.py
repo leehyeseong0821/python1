427ee684b798b39b46ce767d195353fa0067c665
leehyeseong0821 <lhs5567@naver.com>
2022-12-16 20:20:55 +0900

    파이썬 공부

---
+++ 4_1_number.py
@@ -0,0 +1,15 @@
+print(0)
+print(-1)
+print(1)
+print(1.1)
+print('1+1:',1+1)
+print('2-1:',2-1)
+print('2*2:',2*2)
+print('4/2:',4/2)
+
+import math
+print('정답',math.sqrt(4))
+print('정답',math.pow(4,2))
+
+import random
+print(random.random())
---
+++ 4_2_text.py
@@ -0,0 +1,12 @@
+print('Hello world')
+print("Hello world")
+print('''hello
+world ''')
+
+print("'1'+'1'", '1'+'1')
+print("'1'+'1'", 1+1)
+print('1+1:',2)
+print('hello world'*1000)
+print(len('Hello world'*1000))
+
+print('Hello world'.replace('world','universe'))
