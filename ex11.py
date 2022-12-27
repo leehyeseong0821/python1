17a609b28564dde2321aa718eb64606e95aa223b
leehyeseong0821 <lhs5567@naver.com>
2022-12-27 21:55:24 +0900

    파이썬

---
+++ ex11.py
@@ -0,0 +1,39 @@
+# number = 8398333
+
+# if number :
+#     print("처리를 한다")
+# else :
+#     print("0이 나왔습니다.")
+    
+# message=""
+# if message:
+#     print("처리를 한다.")
+# else:
+#     print("메시지를 입력해주세요")
+    
+
+# number1 =0
+
+# if number1 != 0:
+#     pass
+# else:
+#     pass
+
+x=2
+y=10
+if y>2:
+        print(x*y)
+else:
+        print(x+y)
+x=1
+x=4
+if x>4:
+    if y>2:
+        print(x*y)
+    else:
+        print(x+y)
+        
+x= 11
+# if x>10 and x<20:
+if 10< x < 20:
+    print("조건에 맞습니다.")
---
+++ ex12.py
@@ -0,0 +1,27 @@
+year = int(input("태어난 해를 입력해주세요")) %12
+
+if year ==0:
+    print("원숭이 띠입니다.")
+elif year==1:
+    print("토끼 띠입니다.")
+elif year==2:
+    print("뱀 띠입니다.")    
+elif year==3:
+    print("용 띠입니다.")   
+elif year==4:
+    print("쥐 띠입니다.")   
+elif year==5:
+    print("말 띠입니다.")   
+elif year==6:
+    print("돼지 띠입니다.")   
+elif year==7:
+    print("소 띠입니다.")   
+elif year==8:
+    print("양 띠입니다.")   
+elif year==9:
+    print("쥐 띠입니다.")   
+elif year==10:
+    print("개 띠입니다.")   
+elif year==11:
+    print("말 띠입니다.")   
+        
